606e4d2608f8bad0de4083b604024dc41abf6f0d
Márk Mihalik <mihalikmark@gmail.com>
2022-02-12 18:20:59 +0100

    Better code: Function with dict implemented

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-from asyncio.windows_events import NULL
-from cgitb import html
-from ctypes import sizeof
-from os import link
 from bs4 import BeautifulSoup
 import requests
 
-class asset_funds_storage:
-    def __init__(self) -> None:
-        self.name = list()
-        self.link = list()
-        self.status = list()
-
-    def add_name(self, name):
-        self.name.append(name)
-
-    def add_link(self, link):
-        self.link.append(link)
-
-    def add_status(self, status):
-        self.status.append(status)
-
-
-    
 class profit_line_parser:
     def __init__(self) -> None:
-        self.storage = asset_funds_storage()
+        self.list_of_asset = list()
         pass
         
 
@@ -40,12 +19,18 @@ class profit_line_parser:
         rows = BeautifulSoup(str(table), 'html.parser')
         asset_funds = rows.find_all('a')
 
-        for element in asset_funds:
-            self.storage.add_name(element.get_text())
-            self.storage.add_link(element.get('href'))
-        
-        for i in range(len(self.storage.link)):
-            asset_fund_req = requests.get(self.storage.link[i])
+
+        for i in range(len(asset_funds)):
+            temporary_asset_builder = {}
+            temporary_asset_builder['iterator'] = i
+            temporary_asset_builder['name'] = asset_funds[i].get_text()
+            temporary_asset_builder['link'] = asset_funds[i].get('href')
+            temporary_asset_builder['status'] = 'null'
+            self.list_of_asset.append(temporary_asset_builder)
+
+
+        for i in range(len(self.list_of_asset)):
+            asset_fund_req = requests.get(self.list_of_asset[i]['link'])
             parsed_asset_fund = BeautifulSoup(asset_fund_req.text, 'html.parser')
 
             page_useful_content = parsed_asset_fund.find('div', class_="general_box right_ajanlas_ajanlas")
@@ -53,20 +38,20 @@ class profit_line_parser:
             
             recommended_action = recommended_action.split('\xa0')
             recommended_action = recommended_action[1]
-            recommended_action_start = 0
             recommended_action_end = recommended_action.index('\r\n')
-            recommended_action = recommended_action[recommended_action_start:recommended_action_end]
+            recommended_action = recommended_action[0:recommended_action_end]
 
-            self.storage.add_status(recommended_action)
+            self.list_of_asset[i]['status'] = recommended_action
         return
 
     def print_datas(self):
-        for i in range(len(self.storage.name)):
-            print(f'Eszközalap: {self.storage.name[i]}')
-            print(f'Ajánlás: {self.storage.status[i]}')
-            if self.storage.status[i] == 'Vétel':
+        for i in range(len(self.list_of_asset)):
+            print('Eszközalap: ' + self.list_of_asset[i]['name'])
+            print('Ajánlás: ' + self.list_of_asset[i]['status'])
+
+            if self.list_of_asset[i]['status'] == 'Vétel':
                 print('🟩🟩🟩🟩🟩🟩')
-            elif self.storage.status[i] == 'Tartás':
+            elif self.list_of_asset[i]['status'] == 'Tartás':
                 print('🟨🟨🟨🟨🟨🟨')
             else:
                 print('🟥🟥🟥🟥🟥🟥')
